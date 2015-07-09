d04c19d5c2b817e6b9713b7e820911229eb50bb8
ALTree <alb.donizetti@gmail.com>
2015-07-09 16:29:10 +0200

    Moon Image

--- src/main.m
+++ src/main.m
@@ -7,7 +7,7 @@ global encoding;
 
 img = imread('images/lena512.bmp');
 
-dom_range = [16, 32, 64];
+dom_range = [8, 16, 32];
 
 [doms, doms2] = domains(img, dom_range, 1);
 
@@ -19,7 +19,7 @@ drawnow;
 min_range = dom_range(1) / 2;
 max_range = dom_range(length(dom_range)) / 2;
 
-qtfunction(img, [1, 1], length(img), 10, p, min_range, max_range);
+qtfunction(img, [1, 1], length(img), 20, p, min_range, max_range);
 
 
  
