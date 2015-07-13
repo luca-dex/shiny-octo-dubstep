83ac84b2e4c60d0fb3217d16ea93d3df7a5de1b9
luca-dex <luca.desano@gmail.com>
2015-07-13 13:44:37 +0200

    tex

--- src/main.m
+++ src/main.m
@@ -5,14 +5,16 @@ global doms;
 global doms2;
 global encoding;
 
-img = imread('images/shower.bmp');
+img = imread('images/banana.bmp');
 
-dom_range = [4, 8, 16, 32];
+dom_range = [8, 16, 32];
 
 [doms, doms2] = domains(img, dom_range, 1);
 
 min_range = dom_range(1) / 2;
 max_range = dom_range(length(dom_range)) / 2;
+min_rms = 3;
+max_rms = 8;
 
 parpool(4);
 
@@ -36,7 +38,7 @@ parfor i=1:4
             x = length(img) / 2 + 1;
             y = length(img) / 2 + 1;
     end
-    partial_enc = qtfunction(img, [x, y], length(img)/2, 10, min_range, max_range, doms2, 4);
+    partial_enc = qtfunction(img, [x, y], length(img)/2, max_rms, min_range, max_range, doms2, min_rms);
     display '--------------------finito'
     parfor_enc = [parfor_enc ;partial_enc];
 end
